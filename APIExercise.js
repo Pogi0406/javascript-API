373c3a993fcd634be08f8f674288c500faabd6df
IvanPodg <ivan.podgorscak96@gmail.com>
2024-01-08 10:58:45 +0100

    Better solution
    Upload of a new better solution

--- APIExercise.js
+++ APIExercise.js
@@ -1,42 +1,39 @@
-// async function getArtData() {
-//   try {
-//     let responses = await Promise.all([
-//       fetch("https://api.artic.edu/api/v1/artworks/129883"),
-//       fetch("https://api.artic.edu/api/v1/artworks/129884"),
-//       fetch("https://api.artic.edu/api/v1/artworks/129885"),
-//     ]);
-
-//     let jsonData = await Promise.all(
-//       responses.map((response) => response.json())
-//     );
-
-//     console.log(jsonData[0].data.title);
-
-//     let titles = jsonData.map((item) => item.data.title);
-//     console.log(titles);
-//   } catch (error) {
-//     throw new Error("Can't fetch data", error);
-//   }
-// }
-
-// getArtData();
-
 async function getArtData() {
+  const headers = {
+    Authorization: "A8BB3ripjEyB4dUbPuWRQi9GT0XdY99HICdNlZvLNt4lTFVhVXs644rx",
+  };
+
   try {
-    let responses = await Promise.all([
-      fetch("https://api.artic.edu/api/v1/artworks/129883"),
-      fetch("https://api.artic.edu/api/v1/artworks/129884"),
-      fetch("https://api.artic.edu/api/v1/artworks/129872"),
-      fetch("https://api.artic.edu/api/v1/artworks/129880"),
+    const responses = await Promise.all([
+      fetch("https://api.pexels.com/v1/photos/19548535", { headers }),
+      fetch("https://api.pexels.com/v1/photos/19550587", { headers }),
+      fetch("https://api.pexels.com/v1/photos/19557234", { headers }),
+      fetch("https://api.pexels.com/v1/photos/19560882", { headers }),
+      fetch("https://api.pexels.com/v1/photos/19560945", { headers }),
+      fetch("https://api.pexels.com/v1/photos/19561004", { headers }),
     ]);
 
-    let jsonData = await Promise.all(
+    const jsonData = await Promise.all(
       responses.map((response) => response.json())
     );
 
+    console.log(jsonData);
+
     for (let i = 0; i < jsonData.length; i++) {
-      let title = jsonData[i].data.title;
-      document.getElementById(`artTitle${i + 1}`).textContent = title;
+      const photoSrc = jsonData[i].src.medium;
+      const title = jsonData[i].photographer;
+
+      const card = document.createElement("div");
+      card.classList.add("card");
+      card.innerHTML = `<div class="card__header">
+                          <img class="card__image" src="${photoSrc}"/>
+                          </div>
+                          <div class="card__main">
+                            <h3>Photographer:</h3>
+                            <p>"${title}"</p>
+                        </div>`;
+
+      document.body.append(card);
     }
   } catch (error) {
     throw new Error("Can't fetch data", error);
